de92e613ae5fa424b41af6912422caaf55d1d4af
Parul Sharma <parul15123@iiitd.ac.in>
2024-06-21 01:31:43 -0400

    remving clunk from landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://parul-sharma.github.io',
+  base: '/parul-sharma.github.io'
   integrations: [mdx(), sitemap(), tailwind()]
 });
