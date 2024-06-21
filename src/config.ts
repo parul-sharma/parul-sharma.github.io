d25688fc97e8d95dba99288a42d7aa542b7db2a1
parul-sharma <parul15123@iiitd.ac.in>
2024-06-21 16:23:18 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-<<<<<<< HEAD
-export const SITE_TITLE = 'Manuel Ernesto Garcia | Website';
-export const SITE_DESCRIPTION = 'Software Engineer from Cuba • JS • Minimalist • Creando soluciones y compartiendo lo que aprendo.';
-=======
 export const SITE_TITLE = 'Parul Sharma Personal Website';
 export const SITE_DESCRIPTION = 'Bioinformatics Scientist creating tools and pipelines.';
->>>>>>> 2c6c84247f01c6498e5d533d5fd87d16b36980bb
