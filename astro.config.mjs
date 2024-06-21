9d825e4f1f99b01ea69cb718645c9a4808d8e9a3
parul-sharma <parul15123@iiitd.ac.in>
2024-06-21 16:15:06 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://parul-sharma.github.io',
-<<<<<<< HEAD
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
@@ -18,19 +17,3 @@ export default defineConfig({
     }
   )]
 });
-=======
-  integrations: [
-    mdx(), 
-    sitemap(), 
-    tailwind(), 
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }),
-  ],
-  experimental: {
-    viewTransitions: true,
-},
-});
->>>>>>> 2c6c84247f01c6498e5d533d5fd87d16b36980bb
